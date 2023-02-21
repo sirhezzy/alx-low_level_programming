276cdaa7ecca44a2d5e8ed1250d6e375675e5bfc
sirhezzy <hezekiahochoge@gmail.com>
2023-02-21 17:08:26 +0100

    8-24_hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
-* jack_bauer -> every minute of the day 
+* jack_bauer -> every minute of the day
 *
 */
 void jack_bauer(void)
 {
 	int i, j;
+
 	for (i = 0; i < 24; i++)
 	{
 		for (j = 0; j < 60; j++)
