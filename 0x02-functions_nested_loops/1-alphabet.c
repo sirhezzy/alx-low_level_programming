77485dbc9871e84ee81fa6653315105eea8cd7bd
sirhezzy <hezekiahochoge@gmail.com>
2023-02-21 14:24:50 +0100

    putchar printed

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,16 @@
 #include "main.h"
-
 /**
 * main - Entry point
 * print_alphabet -> prints the lowercas alphabets
 * 
 */
-
 void print_alphabet(void);
 {
 	int j;
 
 	for (j = 'a'; j <= 'z'; j++)
 	{
-		putchar(j);
+		_putchar(j);
 	}
 	_putchar('\n');
 }
