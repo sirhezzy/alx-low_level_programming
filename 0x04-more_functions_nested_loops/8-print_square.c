b26d996d7190f2921cac162f41c85c0dbf79f68e
sirhezzy <hezekiahochoge@gmail.com>
2023-02-24 17:09:06 +0100

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_square -> printing line
+* @size: interger parameter
+*/
+void print_square(int size);
+{
+	int x;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
