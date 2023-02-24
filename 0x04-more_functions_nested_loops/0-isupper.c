4f9a839a2e419439ce0618810ba32aa3842fe17e
sirhezzy <hezekiahochoge@gmail.com>
2023-02-24 05:19:30 +0100

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* _isupper -> function checks upper case alphabet
+* @c: an interger parameter
+* Return: success
+*/
+int _is upper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
