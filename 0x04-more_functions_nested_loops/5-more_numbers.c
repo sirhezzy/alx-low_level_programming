e9b777e9fb47dd7e4a1a4be22f960b145a3aeba6
sirhezzy <hezekiahochoge@gmail.com>
2023-02-24 15:02:53 +0100

    more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,11 +11,10 @@ void more_numbers(void)
 	{
 		for (y = 0; y < 15; y++)
 		{
-            {if (y >= 10)
-                    putchar((y / 10) + '0');
-                putchar((y % 10) + '0');
-			}
+			if (y >= 10)
+					_putchar((y / 10) + '0');
+				_putchar((y % 10) + '0');
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 }
