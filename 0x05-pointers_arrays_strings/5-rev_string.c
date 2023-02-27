e144f71527a43ff31dd2a741c15589c42590acee
sirhezzy <hezekiahochoge@gmail.com>
2023-02-27 19:57:49 +0100

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* rev_string -> prints string in reverse
+* @s: string reverse
+* Return: 0
+*/
+
+void rev_string(char *s)
+{
+	int a, half, length;
+	char tmp;
+	
+	for (length = 0; s[length] != '\0'; length++);
+	a = 0;
+	half = length / 2;
+	
+	while (half--)
+	{
+		tmp = s[length - a - 1];
+		s[len - a - 1] = s[a];
+		s[a] = tmp;
+		a++;
+	}
+}
