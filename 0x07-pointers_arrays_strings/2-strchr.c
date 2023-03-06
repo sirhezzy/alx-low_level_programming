08c43424db92bd52faccaa9e483caf7ee0840a7a
sirhezzy <hezekiahochoge@gmail.com>
2023-03-06 16:24:49 +0100

    a function that locates a character in a string.

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,7 +2,7 @@
 /**
  * *_strchr - Locates a character in a string.
  * Description: a function that locates a character
-	in a string.
+ *			in a string.
  * @s: The string to be searched.
  * @c: The character to be located.
  * Return: pointer to first occurance
