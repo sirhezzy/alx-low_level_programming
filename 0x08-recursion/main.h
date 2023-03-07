f5aa8332c6ea0be89c80d0e48456045404f53686
sirhezzy <hezekiahochoge@gmail.com>
2023-03-07 19:41:07 +0100

    5-sqrt_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,6 +6,7 @@ void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
+int find_sqrt(int num, int root);
 int _pow_recursion(int x, int y);
 
 #endif
