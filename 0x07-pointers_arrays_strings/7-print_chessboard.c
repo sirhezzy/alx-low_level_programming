1bb4e1ebc093512f660983147365c6c3d2b7f21b
sirhezzy <hezekiahochoge@gmail.com>
2023-03-07 05:00:48 +0100

    a function that prints the sum of the two

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -5,13 +5,14 @@
  * @a: The chessboard to be printed.
  */
 void print_chessboard(char (*a)[8])
-{
-	int i1, i2;
 
-	for (i1 = 0; a[i][7]; i1++)
+	int index1, index2;
+
+	for (index1 = 0; a[index1][7]; index1++)
 	{
-		for (i2 = 0; i2 < 8; i2++)
-			_putchar(a[i][i2]);
+		for (index2 = 0; index2 < 8; index2++)
+			_putchar(a[index1][index2]);
+
 		_putchar('\n');
 	}
 }
