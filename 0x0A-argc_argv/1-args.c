78fcf1528f60be509a0fefa13bfca5f716a2a0ac
sirhezzy <hezekiahochoge@gmail.com>
2023-03-13 12:44:47 +0100

    Hot_fix

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -18,7 +18,8 @@ int main(int argc, char *argv[])
 		printf("%d\n", argc - 1);
 	else
 	{
-		for (i = 0; *argv; i++, argv++);
+		for (i = 0; *argv; i++, argv++)
+			;
 			printf("%d\n", i - 1);
 	}
 	return (0);
