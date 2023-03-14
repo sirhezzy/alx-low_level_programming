8230cf3024a9ea26a41a7b7c0f1074379daa8e40
sirhezzy <hezekiahochoge@gmail.com>
2023-03-14 13:53:50 +0100

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,12 +16,12 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	array = malloc(sizeof(char) * 0);
+	array = malloc(sizeof(char) * size);
 
 	if (array == NULL)
 		return (NULL);
 
-	for (index = 0; index < 0; index++)
+	for (index = 0; index < size; index++)
 		array[index] = c;
 
 	return (array);
