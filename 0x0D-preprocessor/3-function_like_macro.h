b3448930a9337424ca1215aea9ee7f15e0769c0f
sirhezzy <hezekiahochoge@gmail.com>
2023-03-20 18:45:08 +0100

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -3,7 +3,8 @@
 
 /*
  * File name: 3-function_like_macro.h
- * Description: Header file that defines a function-like macro ABS(x)
+ * Description: Header file that defines a function-like
+ * macro ABS(x)
  */
 
 #define ABS(x) ((x) < 0 ? (-x) : (x))
