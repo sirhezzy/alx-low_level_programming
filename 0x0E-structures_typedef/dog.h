513d833d94fe69fe205b93d657405284828b82ee
Hezekiah Ochoge <hezekiahochoge@gmail.com>
2023-03-20 14:36:27 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-/* * Desc: Header file that defines a new type struct dog.
- */
-
 /**
  * struct dog - A type describing a dog.
  * Desc: A new type describing a dogs infomation
