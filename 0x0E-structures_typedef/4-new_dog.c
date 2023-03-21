4815c4c5353da6526641c215f45799fd099f6ffb
sirhezzy <hezekiahochoge@gmail.com>
2023-03-21 01:25:48 +0100

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -42,6 +42,7 @@ char *_strcopy(char *dest, char *src)
 
 /**
  * new_dog - creates a new dog.
+ * Description: creates info for a new dog.
  * @name: The name of the dog.
  * @age: The age of the dog.
  * @owner: The owner of the dog.
