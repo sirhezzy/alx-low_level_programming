89318c4152706d7f73b96b19e0ff1186324045f5
sirhezzy <hezekiahochoge@gmail.com>
2023-03-22 13:07:36 +0100

    0-print_name.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,10 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
+/**Description: Header file for all prototypes
+ * for project function_pointer
+ */
+
 #include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
