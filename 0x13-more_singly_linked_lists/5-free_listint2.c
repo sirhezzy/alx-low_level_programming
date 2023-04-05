6dbd5f18f90a40fd4087a49d7948ca0394d76f81
sirhezzy <hezekiahochoge@gmail.com>
2023-04-05 12:42:51 +0100

    a function that frees a listint_t list.

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,7 +5,8 @@
 
 /**
  * free_listint2 - frees a list
- * @head, a pointer to the address of the head of a list
+ * @head, a pointer to the address of the head of a list.
+ * Description: a function that frees a listint_t list.
  */
 void free_listint2(listint_t **head)
 {
